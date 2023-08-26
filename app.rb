a7056a956f83868151fcf88cda173c3e08ab98fe
Myles Nottingham <MylesNottingham@gmail.com>
2023-08-26 13:40:46 -0600

    Refactor: Per Rubocop

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
-require 'sinatra'
-require 'sinatra/activerecord'
+require "sinatra"
+require "sinatra/activerecord"
 
 set :database_file, "config/database.yml"
-  
-require './models/cell'
+
+require "./models/cell"
--- models/cell.rb
+++ models/cell.rb
@@ -1,3 +1,2 @@
 class Cell < ActiveRecord::Base
-
 end
