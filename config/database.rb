04863adb04e421cb01c7f35b5941398e02a43ecf
Myles Nottingham <MylesNottingham@gmail.com>
2023-08-28 16:23:59 -0600

    Feat: Create database.rb & environment.rb

---
+++ config/database.rb
@@ -0,0 +1,9 @@
+# set the database based on the current environment
+
+database_name = "backyarder-#{Backyarder.environment}"
+
+# connect ActiveRecord with the current database
+ActiveRecord::Base.establish_connection(
+  :adapter  => "sqlite3",
+  :database => "db/#{database_name}",
+)
