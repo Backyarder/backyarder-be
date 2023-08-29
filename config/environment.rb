83a0b972950ad69fbc9ac5047b9cf2f0a13998fa
Austin Woods <austinwoods99@gmail.com>
2023-08-29 16:50:43 -0500

    remove files from enviorment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,7 @@ APP_ROOT = File.expand_path("..", __dir__)
 
 # require the model(s)
 Dir.glob(File.join(APP_ROOT, "app", "models", "*.rb")).each { |file| require file }
-Dir.glob(File.join(APP_ROOT, 'app', 'controllers', '*.rb')).each { |file| require file }
-
+Dir.glob(File.join(APP_ROOT, "app", "*.rb")).each { |file| require file }
 
 # require the serializer(s)
 # Dir.glob(File.join(APP_ROOT, "app", "serializers", "*.rb")).each { |file| require file }
@@ -23,4 +22,3 @@ class Backyarder < Sinatra::Base
   # set :views, File.join(APP_ROOT, "app", "views")
   # set :public_folder, File.join(APP_ROOT, "app", "public")
 end
-
