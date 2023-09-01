e4bb3de5699f19d75a614bee8787f949482946f2
Boston Lowrey <bostonlowrey@gmail.com>
2023-09-01 14:34:12 -0400

    refactor: needed to change some things to be able to run tests

--- config/environment.rb
+++ config/environment.rb
@@ -7,11 +7,10 @@ APP_ROOT = File.expand_path("..", __dir__)
 # require the controller(s)
 # Dir.glob(File.join(APP_ROOT, "app", "controllers", "*.rb")).each { |file| require file }
 
-# require the model(s)
 Dir.glob(File.join(APP_ROOT, "app", "models", "*.rb")).each { |file| require file }
 Dir.glob(File.join(APP_ROOT, "app", "*.rb")).each { |file| require file }
 
-# require the serializer(s)
+
 Dir.glob(File.join(APP_ROOT, "app", "serializers", "*.rb")).each { |file| require file }
 Dir.glob(File.join(APP_ROOT, "app", "poros", "*.rb")).each { |file| require file }
 Dir.glob(File.join(APP_ROOT, "app", "services", "*.rb")).each { |file| require file }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 ENV["RACK_ENV"] = "test"
 
+require "vcr"
 require "rspec"
+require 'rack/test'
+require 'sinatra'
 
 require "bundler"
 Bundler.require(:default, :test)
@@ -23,4 +26,18 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
+end
+
+VCR.configure do |config|
+  # Set the directory where VCR cassettes will be saved
+  config.cassette_library_dir = 'spec/cassettes'
+
+  # Use :webmock as the HTTP library for handling requests
+  config.hook_into :webmock
+
+  # Enable real HTTP connections if VCR cannot find a cassette
+  config.allow_http_connections_when_no_cassette = false
+
+  # Optionally, filter sensitive information from requests and responses
+  config.filter_sensitive_data('<API_KEY>') { ENV['PERENUAL_API_KEY'] }
 end
