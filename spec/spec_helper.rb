8fe499b013dc043818141fbbf06409042366924c
Boston Lowrey <bostonlowrey@gmail.com>
2023-09-01 15:15:05 -0400

    refactor: vcr and try to add api_key to github actions rspec file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,15 +29,11 @@ RSpec.configure do |config|
 end
 
 VCR.configure do |config|
-  # Set the directory where VCR cassettes will be saved
   config.cassette_library_dir = "spec/cassettes"
-
   # Use :webmock as the HTTP library for handling requests
   config.hook_into :webmock
-
   # Enable real HTTP connections if VCR cannot find a cassette
   config.allow_http_connections_when_no_cassette = false
-
-  # Optionally, filter sensitive information from requests and responses
   config.filter_sensitive_data("<API_KEY>") { ENV["PERENUAL_API_KEY"] }
+  config.debug_logger = $stdout
 end
