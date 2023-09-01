f8d282e88810daba54ba8b446b612b9341673997
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-09-01 12:06:52 -0600

    add PORO for mock plant data

--- app/server.rb
+++ app/server.rb
@@ -11,22 +11,45 @@ class Server < Sinatra::Base
   get "/search/:q" do
     query = params[:q]
     api = PerenualService.new.search(query)[:data]
-    search = api.map do |a|
-      Search.new(a)
+    search = api.map do |result|
+      Search.new(result)
     end
 
-    type = search[0..9].map do |s|
-      next if s.plant_id > 3000
+    type = search[0..9].map do |plant|
+      next if plant.plant_id > 3000
 
-      show = PerenualService.new.detail_search(s.plant_id)
-      s.hardiness = show[:hardiness]
-      s.image = show.dig(:default_image, :thumbnail)
-      s.type = show[:type]
-      s.sunlight = show[:sunlight]
+      show = PerenualService.new.detail_search(plant.plant_id)
+      plant.hardiness = show[:hardiness]
+      plant.image = show.dig(:default_image, :thumbnail)
+      plant.type = show[:type]
+      plant.sunlight = show[:sunlight]
 
-      s
+      plant
     end
 
     json SearchSerializer.new(type)
   end
+
+  get "/index" do
+    json = JSON.parse(File.read("mock_json/index.json"), symbolize_names: true)[:data]
+
+    type = json.map do |result|
+      Plant.new(result)
+    end
+
+    plants = type.map do |plant|
+      next if plant.plant_id > 3000
+
+      plant_details = JSON.parse(File.read("mock_json/details/#{plant[:id]}.json"), symbolize_names: true)[:data]
+      plant.hardiness = show[:hardiness]
+      plant.image = show.dig(:default_image, :thumbnail)
+      plant.type = show[:type]
+      plant.sunlight = show[:sunlight]
+
+      plant
+    end
+
+    json SearchSerializer.new(plants)
+  end
+
 end
