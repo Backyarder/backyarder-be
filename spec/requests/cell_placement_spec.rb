8e97a1afc03fe95f4f06ba6c297ab63ceb6f03db
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-09-02 08:18:03 -0600

    test: add tests for model, remove_plant

--- spec/requests/cell_placement_spec.rb
+++ spec/requests/cell_placement_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "Search Request", type: :request do
   describe "PATCH /cell" do
     it "takes a proper json payload and returns a proper json payload" do
       cell = Cell.create!(location_id: "A1", status: 0)
-      cell_params = { plant_name: "Green Leaf", location_id: "A1", image: "google.com", status: "1", plant_id: "1" }
+      cell_params = { plant_name: "Green Leaf", location_id: "A1", image: "google.com", status: 1, plant_id: 1 }
 
       headers = { "CONTENT_TYPE" => "application/json" }
       response = patch "/cell", JSON.generate(cell_params)
@@ -23,13 +23,30 @@ RSpec.describe "Search Request", type: :request do
                 "image": "google.com",
                 "plant_name": "Green Leaf",
                 "location_id": "A1",
-                "status": 1,
+                "status": "placed",
                 "plant_id": 1,
                 "updated_at": "#{cell.updated_at.strftime("%m/%d/%Y %k:%M")}"
             }
         }
     }
       expect(json_response).to eq(expected)
+    end
+
+    it "removes the plant from the cell and sets the status to empty" do
+      cell = Cell.create!(location_id: "A1", status: 1, plant_id: 1, image: "image_url", plant_name: "Pretty Flower")
+
+      cell_params = { action: 'remove_plant', location_id: "A1" }
+      headers = { "CONTENT_TYPE" => "application/json" }
+
+      response = patch "/cell", JSON.generate(cell_params), headers
+
+      expect(response).to be_successful
+      json_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json_response[:data][:attributes][:status]).to eq('empty')
+      expect(json_response[:data][:attributes][:plant_id]).to be_nil
+      expect(json_response[:data][:attributes][:image]).to be_nil
+      expect(json_response[:data][:attributes][:plant_name]).to be_nil
     end
   end
 end
