e42b2232bd44ad1c4c88c9b5744fa13f9e8add91
Myles Nottingham <MylesNottingham@gmail.com>
2023-09-02 14:31:37 -0600

    Feat: Add endpoint to delete all placed plants

--- app/server.rb
+++ app/server.rb
@@ -78,4 +78,8 @@ class Backyarder < Sinatra::Base
 
     json CellSerializer.new(cell)
   end
+
+  delete "/garden" do
+    Cell.where(status: :placed).update_all(status: :empty, plant_id: nil, image: nil, plant_name: nil)
+  end
 end
