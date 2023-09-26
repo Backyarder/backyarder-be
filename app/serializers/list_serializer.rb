b6403ed6821f387b1ea256fe4b25680872facf50
Myles Nottingham <MylesNottingham@gmail.com>
2023-09-26 10:52:02 -0600

    Feat: Add watering to list serializer

--- app/serializers/list_serializer.rb
+++ app/serializers/list_serializer.rb
@@ -1,4 +1,4 @@
 class ListSerializer
   include JSONAPI::Serializer
-  attributes :image, :plant_name, :hardiness, :type, :sunlight, :plant_id
+  attributes :image, :plant_name, :hardiness, :type, :sunlight, :watering, :plant_id
 end
