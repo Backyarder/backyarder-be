73c1123ecfc2a455a7ed31d2ceeb05542095ee9f
Boston Lowrey <bostonlowrey@gmail.com>
2023-10-14 13:12:59 -0400

    refactor: decor item poro spec

--- app/poros/decor_item.rb
+++ app/poros/decor_item.rb
@@ -2,7 +2,7 @@ class DecorItem
   attr_accessor :image_url, :name, :type, :content_type
 
   attr_reader :id
-  
+
   def initialize(attributes)
     @id = nil
     @image_url = attributes[:attributes][:image_url]
